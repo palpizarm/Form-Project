0690eab42b592ecb0d341a07eeafa290aa896c49
palpizarm <pabloalpizar99@gmail.com>
2021-10-18 14:24:28 -0600

    [new] add api configuration

---
+++ API-REST/app.js
@@ -0,0 +1,26 @@
+//imports
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+
+// Middleware
+app.use(
+    express.urlencoded({
+        extended : true
+    })
+);
+app.use(express.json());
+
+// routes
+
+
+mongoose.connect(process.env.DB_CONNECTION,
+    {useUnifiedTopology: true, useNewUrlParser: true},
+    () => {console.log('Connected to DB!!!')
+    });
+
+// Server listening in port 3000
+app.listen(3000);
