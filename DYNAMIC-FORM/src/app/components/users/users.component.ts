a4f245624c44a4e304119ff33f83b3ec3ea52b71
palpizarm <pabloalpizar99@gmail.com>
2021-10-20 12:20:15 -0600

    [improved] show users in a table

--- DYNAMIC-FORM/src/app/components/users/users.component.ts
+++ DYNAMIC-FORM/src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+
+import $ from 'jquery';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  usersList: User[] = [];
+  
+
   constructor() { }
 
   ngOnInit(): void {
+      // users dummy
+      this.usersList.push(<User>{name:'Juan', password : 'Juan88', typeUser: 'usuario', forms: ['Vacaciones'], email: ''}) 
+      this.usersList.push(<User>{name:'Juan1', password : 'Juan88', typeUser: 'usuario', forms: ['Vacaciones'], email: ''}) 
+      this.usersList.push(<User>{name:'Juan2', password : 'Juan88', typeUser: 'usuario', forms: ['Vacaciones'], email: ''}) 
+    }
+
+
+
+  loadData(){
+    $('#users-table').DataTable();
   }
+
+
 
 }
--- DYNAMIC-FORM/src/app/models/user.model.ts
+++ DYNAMIC-FORM/src/app/models/user.model.ts
@@ -2,4 +2,6 @@ export class User {
     email : string;
     password : string;
     name : string;
+    typeUser: string;
+    forms: String[];
 }
