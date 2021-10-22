79b56a751ea5335e6b2b3901ef040c4053a77551
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2021-10-21 20:23:43 -0600

    Completion of all methods in the FormR (Form route))

--- API-REST/routes/FormR.js
+++ API-REST/routes/FormR.js
@@ -95,3 +95,138 @@ router.get('/toReview/:receiver',async (req, res)=>{//Los que tiene que revisar
         });
     }
 });
+
+/*
+METHOD: GET
+Description: get all form by sender (user)
+*/
+router.get('/:sender',async (req, res)=>{
+    try{
+        const user=req.params.sender;
+        const form = await Form.find({sender:user});
+        res.json({
+            errorCode: 0,
+            errorMsg: "",
+            data :form
+        });
+    }catch(error){
+        res.json({
+            errorCode: -1,
+            errorMsg: "Error inesperado",
+            data : error
+        });
+    }
+});
+
+
+router.patch('/approve',async (req, res)=>{//Aprobar
+    try{
+        
+        const pReceiver=req.body.receiver;
+        const pId=req.body.id;
+        const formC = await Form.count({_id:pId});
+
+        if(formC>0){
+
+            const form = await Form.findOne({_id:pId});
+            var pApprovers=form.approvers;
+            pApprovers.unshift(pReceiver);
+            const longApprovers = pApprovers.length;
+            const pApprovalsRequired= (await ApprovalRoute.findOne({sender:form.sender, formTitle:form.title})).approvalsRequired;
+            
+
+            if(longApprovers == pApprovalsRequired){
+                const formUpdated = await Form.updateOne({_id:pId},{approvers:pApprovers,state:"Aceptado"});
+                res.json({
+                    errorCode: 0,
+                    errorMsg: "",
+                    data : formUpdated
+                });
+            }else{
+                const formUpdated = await Form.updateOne({_id:pId},{approvers:pApprovers});
+                res.json({
+                    errorCode: 0,
+                    errorMsg: "",
+                    data : formUpdated
+                });
+            }
+        }else{
+            res.json({
+                errorCode: -1,
+                errorMsg: "No se encontro el form.",
+                data : ""
+            });
+        }
+    }catch(error){
+        res.json({
+            errorCode: -1,
+            errorMsg: "Error inesperado",
+            data : error
+        });
+    }
+});
+
+router.patch('/reject',async (req, res)=>{//Rechazar
+    try{
+
+        const pReceiver=req.body.receiver;
+        const pId=req.body.id;
+        const formC = await Form.count({_id:pId});
+
+        if(formC>0){
+
+            const form = await Form.findOne({_id:pId});
+            var pRejecters=form.rejecters;
+            pRejecters.unshift(pReceiver);
+            const longRejecters = pRejecters.length;
+            const pApprovalsRequired= (await ApprovalRoute.findOne({sender:form.sender, formTitle:form.title})).approvalsRequired;
+            const longReceivers=form.receivers.length
+
+            if((longReceivers - longRejecters) < pApprovalsRequired){
+                const formUpdated = await Form.updateOne({_id:pId},{rejecters:pRejecters,state:"Rechazado"});
+                res.json({
+                    errorCode: 0,
+                    errorMsg: "",
+                    data : formUpdated
+                });
+            }else{
+                const formUpdated = await Form.updateOne({_id:pId},{rejecters:pRejecters});
+                res.json({
+                    errorCode: 0,
+                    errorMsg: "",
+                    data : formUpdated
+                });
+            }
+        }else{
+            res.json({
+                errorCode: -1,
+                errorMsg: "No se encontro el form.",
+                data : ""
+            });
+        }
+    }catch(error){
+        res.json({
+            errorCode: -1,
+            errorMsg: "Error inesperado",
+            data : error
+        });
+    }
+});
+
+router.delete('/deleteId',async (req, res)=>{
+    try{
+        const pId=req.body.id;
+        const appRoute = await Form.deleteOne({_id:pId});
+        res.json({
+            errorCode: 0,
+            errorMsg: "",
+            data : appRoute
+        });
+    }catch(error){
+        res.json({
+            errorCode: -1,
+            errorMsg: "Error inesperado",
+            data : error
+        });
+    }
+});
