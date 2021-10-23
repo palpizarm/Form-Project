ff35fca9df198d9923c2afa9bd12dee9a4055cc7
palpizarm <pabloalpizar99@gmail.com>
2021-10-23 00:11:44 -0600

    [new] add a form-template page

--- DYNAMIC-FORM/src/app/app-routing.module.ts
+++ DYNAMIC-FORM/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FormListComponent } from './components/form-list/form-list.component';
+import { FormTemplateComponent } from './components/form-template/form-template.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { UsersComponent } from './components/users/users.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'accounts/users', component: UsersComponent},
   {path: 'accounts/new', component: NewUserComponent},
   {path: 'forms', component: FormListComponent},
+  {path: 'create-form', component: FormTemplateComponent},
 
   {path: '**', pathMatch:'full', redirectTo: 'home'}
 ];
