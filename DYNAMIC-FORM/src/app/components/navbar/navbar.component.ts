61024ebd04814ccaa424ca79904a424aa0b0a8ed
palpizarm <pabloalpizar99@gmail.com>
2021-10-23 21:34:14 -0600

    [fixed] fix function to log out

--- DYNAMIC-FORM/src/app/components/navbar/navbar.component.ts
+++ DYNAMIC-FORM/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,18 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   adminUser : Boolean = true;
-  constructor() { }
+  
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  // first remove from localStorage item of the log 
+  logout(){
+    localStorage.removeItem('user');
+    localStorage.removeItem('userType');
+    localStorage.removeItem('logDate');
+    this.router.navigateByUrl('/login');
   }
 
 }
